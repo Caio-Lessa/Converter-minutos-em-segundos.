1230f55badf90c97d461a21ec076be467f62a5b1
Caio Rocha Lessa <0040482321025@americana.fatec.br>
2024-03-21 08:46:37 -0300

    first commit

--- Untitled1.c
+++ Untitled1.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main ()
 {
-int segundos, minutos;
-printf("Digite os minutos que deseja converter em segundos:\n");
-scanf("%d", &minutos);
-segundos = 60 * minutos;
-printf("A conversao e: %d", segundos);
+float num1, num2, add, sub, mult, div;
+printf("Digite dois numeros para realizar as 4 operacoes entre eles;\n");
+scanf("%f%f", &num1, &num2);
+add = num1 + num2;
+sub = num1 - num2;
+mult = num1 * num2;
+div = num1 / num2;
+printf("Resultados - Adicao: %f, Subtracao: %f, Multiplicacao: %f, Divisao: %f", add, sub, mult, div);
 return 0;
 }
